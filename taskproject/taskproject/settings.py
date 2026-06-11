146b314eaef649650a399291b2bf0c22b1d29a99
SATHYA SARAVANAN <sathya>
2026-06-11 10:54:25 +0530

    sqlite to mysql db control

--- taskproject/taskproject/settings.py
+++ taskproject/taskproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'taskproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taskdb',
+        'USER':'root',
+        'PASSWORD':'sathya',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
